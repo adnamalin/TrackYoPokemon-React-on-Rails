703293ad5e7949252b01e57394aec9da7ee4746f
AMANDA LIN <AmandaLin@AMANDAs-MacBook-Air.local>
2016-07-17 22:43:04 -0500

    postgres

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20160717004945) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "pokemons", force: :cascade do |t|
     t.string   "name"
     t.date     "date"
